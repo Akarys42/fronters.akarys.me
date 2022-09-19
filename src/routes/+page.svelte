b7d02b237abbbabf06d581d2e8554425a57dd77c
Amber Bertucci <amber@akarys.me>
2022-09-19 23:14:15 +0200

    staging

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
     import { onMount } from 'svelte';
 
-    import GlobalStyle from '$lib/styles/global.scss';
     import Front from '$lib/components/cards/Front.svelte';
     import { buildSwitchOutText, buildFrontPageTitle, buildFrontEmbedDescription, buildFrontEmbedTitle, getAvatar, getColor } from '$lib/functions/strings';
 
@@ -10,6 +9,7 @@
     let url = "";
     onMount(() => url = window.location.href);
 </script>
+<style src="lib/styles/_style.scss">a {}</style>
 
 <h2>{buildFrontPageTitle(data.members)}</h2>
 <div class="front container">
@@ -31,4 +31,5 @@
     <meta property="og:url" content={url} />
     <meta property="og:image" content={data.members && data.members.length > 0 ? getAvatar(data.members[0]) : ""} />
     <meta name="theme-color" content={data.members && data.members.length > 0 ? getColor(data.members[0], true) : ""}>
+    <link rel="stylesheet" href="./lib/styles/dark.css" media="(prefers-color-theme: dark)" />
 </svelte:head>
